7cd8a8107642b4f40a2f6d3c235410977221d6e1
thu.huu.tran <thu.huu.tran@topicus.nl>
2021-07-20 09:14:42 +0700

    user story 3 done
    user story 3 done

--- src/app/features/transaction/transaction/transaction.component.ts
+++ src/app/features/transaction/transaction/transaction.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
     ]
 } )
 export class TransactionComponent {
+    handleTransferCreatedEvent( transferId: number ): void {
+        console.log( transferId );
+    }
 }
