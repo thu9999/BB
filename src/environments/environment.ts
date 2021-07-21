41469d5624f7b57560bfe3d42609d4897fbb4e15
thu.huu.tran <thu.huu.tran@topicus.nl>
2021-07-21 14:28:45 +0700

    set environment to dev
    set environment to dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: true,
+    production: false,
     baseUrl: 'https://r9vdzv10vd.execute-api.eu-central-1.amazonaws.com'
 };
 
